73e3fe55b530e442668ddb037f7fb4208c3e7734
Fernando Miguel <fer_lop10@hotmail.com>
2024-09-09 12:19:47 +0200

    update

---
+++ src/extract_load.py
@@ -0,0 +1,20 @@
+import yfinance as yf
+import pandas as pd
+import os
+from dotenv import load_dotenv
+from sqlalchemy import create_engine
+
+commodities = ['CL=F', 'GC=F', 'SI=F']
+
+def search_commodities(simbolo, period='5d', interval='1d'):
+    ticker = yf.Ticker(simbolo)
+    dados = ticker.history(period=period, interval=interval)['Close']
+    dados['ticker'] = simbolo
+    return dados
+
+def search_all_data_commodities():
+    all_data = []
+    for commodity in commodities:
+        dados = search_commodities(commodity)
+        all_data.append(dados)
+    return pd.concat(all_data)
